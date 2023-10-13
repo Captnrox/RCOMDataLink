b9b2a4b2ca179d6437a5ff39253b91504b0d55c2
Rita Lopes <up202108819@up.pt>
2023-10-13 12:40:28 +0100

    Added stuffing and destuffing mechanism

--- src/application_layer.c
+++ src/application_layer.c
@@ -5,5 +5,11 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    // llopen()
+
+    // Transmissor: llwrite() -> antes de escrever dá stuff and bytes (feito no PROTOCOL) (997 bytes por chunk)
+    // Primeiro manda um control  packet (que é uma information frame) com o tamanho e nome do ficheiro
+    // Recetor: llread() -> depois de ler os dados são destuffed (feito no PROTOCOL)
+
+    unsigned char result[2 * n + 8]; // If we hypothetically have to escape every byte of the array, it will take up 2n. The header and ending take 6 bytes, or 8 if we have to stuff the BCCs
 }
